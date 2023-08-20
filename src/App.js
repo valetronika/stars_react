8891e98ea5c96252e033b44b2e7ae6e10df038ca
valetronika <valentina2wo@gmail.com>
2023-08-20 16:34:08 +0200

    edit hw 17.08

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Products from './components/Products/Products';
 import Footer from './components/footer/Footer/Footer';
 import Header from './components/header/Header';
 function App() {
+  console.log('++++++++++++++++++');
   return (
     <div className="App">
       <Header/>
